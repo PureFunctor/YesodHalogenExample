78dfc8cbea44feacbb49faebae7677cbc0720c3a
PureFunctor <purefunctor@gmail.com>
2020-12-07 00:08:22 +0800

    [PURS] Add action handler for the forms component

--- halogen_frontend/src/Colors.purs
+++ halogen_frontend/src/Colors.purs
@@ -1,6 +1,8 @@
 module Colors where
 
-import Data.Show (class Show)
+import Prelude
+
+import Halogen as H
 
 data RGB = Red | Green | Blue
 
@@ -10,8 +12,17 @@ instance showRGB :: Show RGB where
   show Blue = "Blue"
 
 
-type FormsState = { current :: { r :: Int , g :: Int , b :: Int } }
+type RGBValues = { r :: Int , g :: Int , b :: Int }
+type FormsState = { current :: RGBValues }
+data FormsAction = FormSubmission
 
 
 formsInitialState :: forall input. input -> FormsState
 formsInitialState _ = { current : { r : 0 , g : 0  , b : 0 } }
+
+
+formsHandleAction :: forall m. FormsAction -> H.HalogenM FormsState FormsAction () RGBValues m Unit
+formsHandleAction = case _ of
+  FormSubmission -> do
+    { current } <- H.get
+    H.raise current
