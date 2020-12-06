bd21c3444f580614ba9e292f6e1bb9f78b2c59bc
PureFunctor <purefunctor@gmail.com>
2020-12-06 18:57:47 +0800

    [PURS] Add event and helper for ChangeColor

--- halogen_frontend/src/Home.purs
+++ halogen_frontend/src/Home.purs
@@ -3,8 +3,11 @@ module Home where
 import Prelude
 
 import Data.Array (singleton)
+import Data.Int (fromString)
+import Data.Maybe (Maybe(..), fromMaybe)
 import Halogen as H
 import Halogen.HTML as HH
+import Halogen.HTML.Events as HE
 import Halogen.HTML.Properties as HP
 import Utils (css)
 
@@ -29,12 +32,22 @@ mkTitle = HH.p [ css "title has-text-dark" ] <<< singleton
 mkSubtitle :: forall w i. HH.HTML w i-> HH.HTML w i
 mkSubtitle = HH.p [ css "subtitle has-text-dark" ] <<< singleton
 
-colorField :: forall w i. String -> HH.HTML w i
+changeColor ::  String -> String -> Maybe Action
+changeColor name value = Just $ ChangeColor name (fromMaybe 0 <<< fromString $ value)
+
+colorField :: forall w. String -> HH.HTML w Action
 colorField color =
   HH.div [ css "field" ]
   [ HH.label [ css "label" ] [ HH.text color ]
   , HH.div [ css "control" ]
-    [ HH.input [ css "input", HP.type_ HP.InputNumber, HP.placeholder "0~255", HP.min 0.0, HP.max 255.0 ] ]
+    [ HH.input [ css "input"
+               , HP.type_ HP.InputNumber
+               , HP.placeholder "0~255"
+               , HP.min 0.0
+               , HP.max 255.0
+               , HE.onValueChange $ changeColor color
+               ]
+    ]
   ]
 
 colorFieldSubmit :: forall w i. HH.HTML w i
