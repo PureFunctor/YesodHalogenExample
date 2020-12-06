674ed73164ee97286f7ebde5442e03c232e206d4
PureFunctor <purefunctor@gmail.com>
2020-12-06 23:59:02 +0800

    [PURS] Add the state type to be used for the forms

--- halogen_frontend/src/Colors.purs
+++ halogen_frontend/src/Colors.purs
@@ -8,3 +8,10 @@ instance showRGB :: Show RGB where
   show Red = "Red"
   show Green = "Green"
   show Blue = "Blue"
+
+
+type FormsState = { current :: { r :: Int , g :: Int , b :: Int } }
+
+
+formsInitialState :: forall input. input -> FormsState
+formsInitialState _ = { current : { r : 0 , g : 0  , b : 0 } }
