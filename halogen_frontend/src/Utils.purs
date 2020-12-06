78134d70fa987398df470d9b456408f264714284
PureFunctor <purefunctor@gmail.com>
2020-12-06 12:32:07 +0800

    [PURS] Add utility for adding CSS classes

---
+++ halogen_frontend/src/Utils.purs
@@ -0,0 +1,10 @@
+module Utils where
+
+import Prelude
+
+import Halogen.HTML as HH
+import Halogen.HTML.Properties as HP
+
+-- Utility function for adding a CSS class
+css :: forall r i. String -> HP.IProp (class :: String | r) i
+css = HP.class_ <<< HH.ClassName
