8e48d837289941a2cdba8aa27333701ff8a3efed
PureFunctor <purefunctor@gmail.com>
2020-12-06 13:04:02 +0800

    [PURS] Add the initial layout for the home page

--- halogen_frontend/src/Home.purs
+++ halogen_frontend/src/Home.purs
@@ -5,6 +5,8 @@ import Prelude
 import Halogen as H
 import Halogen.HTML as HH
 
+import Utils (css)
+
 type State = Unit
 type Action = Unit
 
@@ -15,4 +17,29 @@ initialState :: forall input. input -> State
 initialState _ = unit
 
 render :: forall m. State -> H.ComponentHTML Action () m
-render _ = HH.div_ [ HH.h1_ [ HH.text "Hello, World"] ]
+render _ =
+  HH.div [ css "hero is-dark is-fullheight" ]
+  [
+    HH.div [ css "hero-body" ]
+    [
+      HH.div [ css "tile is-ancestor" ]
+      [
+        HH.div [ css "tile is-parent" ]
+        [
+          HH.div [ css "tile is-child notification is-light" ]
+          [
+            HH.p [ css "title has-text-dark" ] [ HH.text "Input Here" ]
+          , HH.p [ css "subtitle has-text-dark" ] [ HH.text "All told, a monad in X is just a monoid in the category of endofunctors of X, with product × replaced by composition of endofunctors and unit set by the identity endofunctor." ]
+          ]
+        ]
+      , HH.div [ css "tile is-parent" ]
+        [
+          HH.div [ css "tile is-child notification is-light" ]
+          [
+            HH.p [ css "title has-text-dark" ] [ HH.text "Output Here" ]
+          , HH.p [ css "subtitle has-text-dark" ] [ HH.text "All told, a monad in X is just a monoid in the category of endofunctors of X, with product × replaced by composition of endofunctors and unit set by the identity endofunctor." ]
+          ]
+        ]
+      ]
+    ]
+  ]
