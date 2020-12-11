003ec29af42fab7f1c524c2ae76b67e10da55f3a
PureFunctor <purefunctor@gmail.com>
2020-12-11 22:59:15 +0800

    [PURS] Add the About page

---
+++ halogen_frontend/src/About.purs
@@ -0,0 +1,30 @@
+module About where
+
+import Prelude
+
+import Halogen as H
+import Halogen.HTML as HH
+import Utils (css)
+
+
+type State = Unit
+
+
+component :: forall query input output m. H.Component HH.HTML query input output m
+component =
+  H.mkComponent
+  { initialState
+  , render
+  , eval: H.mkEval $ H.defaultEval
+  }
+  where
+  initialState :: input -> Unit
+  initialState _ = unit
+
+  render :: forall action. State -> H.ComponentHTML action () m
+  render _ =
+    HH.div [ css "hero is-dark is-fullheight" ]
+    [ HH.div [ css "hero-body" ]
+      [ HH.h1_ [ HH.text "About Page" ]
+      ]
+    ]
